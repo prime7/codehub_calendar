c93d7402c1160c63fc45bbf6341657641286283f
Daniil <denngall2@gmail.com>
2020-05-21 21:44:16 -0700

    Submit button removed from the second page to the first one, added setInterval to move function

--- functions/ShowCourses.js
+++ functions/ShowCourses.js
@@ -2,33 +2,45 @@ var i = 0;
 
 function move() {
 
+    $("#carouselExampleIndicators").carousel(1);
+
     document.getElementById('backButton').disabled = true;
 
-    if (i == 0) {
-        i = 1;
-        var elem = document.getElementById("myBar");
-        var width = 1;
-        var id = setInterval(frame, 10);
+    setTimeout(function () {
 
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-                i = 0;
-                document.getElementById('backButton').disabled = false;
-                next();
-            } else {
-                width++;
-                elem.style.width = width + "%";
-                elem.innerHTML = width + "%";
+        if (i == 0) {
+            i = 1;
+            var elem = document.getElementById("myBar");
+            var width = 1;
+            var id = setInterval(frame, 10);
+
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                    i = 0;
+                    document.getElementById('backButton').disabled = false;
+                    setTimeout(next, 1500);
+                } else {
+                    width++;
+                    elem.style.width = width + "%";
+                    elem.innerHTML = width + "%";
+                }
             }
-        }
-    }
+        }}, 1500);
     
 }
 
 function next(){
     $("#carouselExampleIndicators").carousel(2);
     addCourseForms();   
+}
+
+function acceptCourses(){
+
+    // read all values
+
+    // then go to another slide
+    $("#carouselExampleIndicators").carousel(3);
 }
 
 var selDiv = "";

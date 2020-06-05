a6c7ced9eca2b5f4ce4e059b0f800389edd4fa63
agam23 <57159521+agam23@users.noreply.github.com>
2020-06-05 10:34:07 -0700

    Update testCode.py

--- server/testTabula/testCode.py
+++ server/testTabula/testCode.py
@@ -1,54 +1,55 @@
 import csv
 import tabula
 import json
+import re
 
 # convert PDF into CSV file
 #tabula.convert_into("testInput.pdf", "output.csv", output_format="csv", pages='all')
 
-courseCode = "CSIS 3175"
+courseCode = ""
+
+coursePattern = re.compile(r"\b(CSIS) [0-9]{4}\b", flags=re.IGNORECASE)
+
+datePattern = re.compile(
+    r"(\b[0-9]{1,2} Mar)|((Jan|Feb|Mar|Apr|May|June|July|Aug) [0-9]{1,2}\b)", flags=re.IGNORECASE)
+
+eventPattern = re.compile(
+    r"(Quiz|Assignment|midterm|final|Project|presentation)", flags=re.IGNORECASE)
 
 dictForDatesAndEvent = {}
 # refining input
 with open('output.csv', newline='') as csvfile:
     fileReader = csv.reader(csvfile, delimiter=',')
-    line = 0
 
     # going through each row
     for row in fileReader:
-        print(f'line number : {line}')
 
-        #copyOfRow = row
         # Going through each coloumn in each row
         for colToCheckEvent in row:
-            print(colToCheckEvent)
 
-            """
-            Direct comparision is made to find out if event is being mentioned.
-            Needs to be change to make it dynamic.
-            Suggestio - Reg Expression
-            """
+            # checking for course number as well
+            if(courseCode == ""):
+                course = coursePattern.search(colToCheckEvent)
+                if(course is not None):
+                    courseCode = course.group()
 
-            # TO DO
-            if (colToCheckEvent == "MIDTERM" or ("Project Presentation" in colToCheckEvent)):
+            event = eventPattern.search(colToCheckEvent)
+
+            if (event is not None):
 
                 # Since we found a event in current row, hence we will go through it again to find date
                 for colToCheckDate in row:
 
+                    date = datePattern.search(colToCheckDate)
                     # TO DO
-                    if (colToCheckDate == "Feb 28" or ("Apr 1" in colToCheckDate)):
-                        dictForDatesAndEvent[colToCheckEvent] = colToCheckDate
+                    if (date is not None):
+                        dictForDatesAndEvent[event.group()] = date.group()
 
-        line += 1
-
-print(dictForDatesAndEvent)
-
-#result dictionary of events, dates, course name, course code
+# result dictionary of events, dates, course name, course code
 resultDict = {
     "Course Code": courseCode,
-    "Events": dictForDatesAndEvent
+    "EventsAndDates": dictForDatesAndEvent
 }
-
-print(resultDict)
 
 jsonResponse = json.dumps(resultDict)
 
